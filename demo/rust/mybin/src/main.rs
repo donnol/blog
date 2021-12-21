abcf8fe057d8c24afccd4b5982c89e7ad85d5c89
donnol <donnol@outlook.com>
2021-12-21 11:42:42 +0800

    demo/rust: add const fn

--- demo/rust/mybin/src/main.rs
+++ demo/rust/mybin/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
+
+    println!("{}", add(1, 2));
+    println!("{}", add1(1, 2));
+
+    // const fn set value to a const var
+    const V: u8 = add(2, 4);
+    println!("{}", V);
+
+    // error[E0015]: calls in constants are limited to constant functions, tuple structs and tuple variants
+    // const v1: u8 = add1(4, 3);
+    // println!("{}", v1)
+}
+
+const fn add(x: u8, y: u8) -> u8 {
+    x+y
+}
+
+fn add1(x: u8, y: u8) -> u8 {
+    x+y
 }
