33811012d2bffe23350749ea4c0da58a353367e1
donnol <donnol@outlook.com>
2021-12-23 15:11:10 +0800

    demo/java/: add properties value

--- demo/java/my-spring-boot/src/main/java/com/jdscript/spring/MyspringApplication.java
+++ demo/java/my-spring-boot/src/main/java/com/jdscript/spring/MyspringApplication.java
@@ -1,10 +1,24 @@
 package com.jdscript.spring;
 
+import javax.annotation.PostConstruct;
+
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MyspringApplication {
+	@Value("${my.user.name}") // ${user.name} 获取的是服务器的当前登录用户名称
+	private String name;
+
+	private void printName() {
+		System.out.println("===== name: "+this.name);
+	}
+
+	@PostConstruct // executed after dependency injection is done -- 依赖注入后执行
+	public void init (){
+ 		this.printName() ;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyspringApplication.class, args);
