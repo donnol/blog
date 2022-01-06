353679a7348e60ffe0f108d5f390085cec6612f2
donnol <donnol@outlook.com>
2022-01-06 17:28:30 +0800

    demo/java: add Retention to annotation

--- demo/java/my-spring-boot/src/main/java/com/jdscript/spring/annotation/MyAnnotation.java
+++ demo/java/my-spring-boot/src/main/java/com/jdscript/spring/annotation/MyAnnotation.java
@@ -1,6 +1,10 @@
 package com.jdscript.spring.annotation;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 // 定义注解
+@Retention(RetentionPolicy.RUNTIME) // 没有指定'RUNTIME'的话，反射会拿不到
 public @interface MyAnnotation {
     String value() default "this is an annotation by me";
     Class<? extends Object> classValue();
