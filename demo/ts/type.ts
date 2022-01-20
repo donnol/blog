59b2aa4c0f2cb93cdb10e0c7757378bf17d73337
donnol <donnol@outlook.com>
2022-01-20 14:47:09 +0800

    demo/ts: add extends ? and infer

--- demo/ts/type.ts
+++ demo/ts/type.ts
@@ -11,3 +11,48 @@ function printm(m: M): U {
     console.log(m);
     return 0;
 }
+
+type MK<T> = T extends void ? {
+    more?: boolean
+} : {
+    more?: boolean
+    params: T
+}
+
+export class MultiPager<T1, T2 = void> {
+    fetch = (opts: MK<T2>) => {
+        console.log(opts)
+    }
+}
+
+new MultiPager<string>().fetch({ more: true })
+new MultiPager<string, number>().fetch({ more: true, params: 1 })
+
+export class MultiPager2<T1, T2 = void> {
+    fetch = (opts: { more?: boolean, params?: T2 }) => { // 用params?会导致范围变大
+        console.log(opts)
+    }
+}
+
+new MultiPager2<string>().fetch({ more: true })
+new MultiPager2<string>().fetch({ more: true, params: void (1) })
+new MultiPager2<string, number>().fetch({ more: true })
+new MultiPager2<string, number>().fetch({ more: true, params: 1 })
+
+// 根据传入的类型，选择返回的类型
+// 根据传入的类型，提取它里面的子类型并返回
+
+// without infer
+type Ids = number[];
+type Names = string[];
+
+type Unpacked<T> = T extends Names ? string : T extends Ids ? number : T;
+
+type idType = Unpacked<Ids>; // idType 类型为 number
+type nameType = Unpacked<Names>; // nameType 类型为string
+
+// with infer
+type Unpacked2<T> = T extends (infer R)[] ? R : T;
+
+type idType2 = Unpacked<Ids>; // idType 类型为 number
+type nameType2 = Unpacked<Names>; // nameType 类型为string
