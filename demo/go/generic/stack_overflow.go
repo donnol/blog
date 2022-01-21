746c7d04adc10344db432377e3900c37de064b1a
donnol <donnol@outlook.com>
2022-01-21 10:11:54 +0800

    demo/go: update generic

--- demo/go/generic/stack_overflow.go
+++ demo/go/generic/stack_overflow.go
@@ -1,6 +1,11 @@
 package generic
 
 // 编译时，类型检查`递归调用`时栈溢出
-// func f[P any](a, _ P) {
-// 	f(a, int(0))
-// }
+// 
+// 使用`go version devel go1.18-d15481b Fri Jan 21 01:14:28 2022 +0000 linux/amd64`编译时，不再报溢出错误；
+// 但是，会报：type int of int(0) does not match P
+// 
+// > With the above CL this doesn't crash anymore. Still needs a proper fix but not a release blocker anymore. [From](https://github.com/golang/go/issues/48656)
+func f[P any](a, _ P) {
+	f(a, int(0))
+}
