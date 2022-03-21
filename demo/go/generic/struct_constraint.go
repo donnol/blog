873a5d2070b8d45fbd179bd0284b27439bc51cb2
donnol <donnol@outlook.com>
2022-03-21 20:58:30 +0800

    demo/go/: update go version, comment generic struct contraint

--- demo/go/generic/struct_constraint.go
+++ demo/go/generic/struct_constraint.go
@@ -1,13 +1,9 @@
 package generic
 
-import (
-	"fmt"
-)
-
 // https://github.com/golang/go/issues/50417
 
 type Condition[T ~string, S ~string] interface {
-	~struct{
+	~struct {
 		Type               T
 		Status             string
 		Severity           S
@@ -20,9 +16,18 @@ type Condition[T ~string, S ~string] interface {
 // structField is a type constraint whose type set consists of some
 // struct types that all have a field named x.
 type structField interface {
-	struct { a int; x int } |
-		struct { b int; x float64 } |
-		struct { c int; x uint64 }
+	struct {
+		a int
+		x int
+	} |
+		struct {
+			b int
+			x float64
+		} |
+		struct {
+			c int
+			x uint64
+		}
 }
 
 // This function is INVALID.
@@ -33,16 +38,25 @@ type structField interface {
 // }
 
 type structFieldSameX interface {
-	struct { a int; X int } |
-		struct { b int; X int } |
-		struct { c int; X int }
+	struct {
+		a int
+		X int
+	} |
+		struct {
+			b int
+			X int
+		} |
+		struct {
+			c int
+			X int
+		}
 }
 
 // 同样类型的x字段，还是不行呢: go version devel go1.18-07525e1 Fri Jan 7 00:15:59 2022 +0000 linux/amd64
 // func IncrementSameX[T structFieldSameX](p *T) {
-// 	v := p.X 
+// 	v := p.X
 // 	v++
-// 	p.X = v 
+// 	p.X = v
 // }
 
 type Cat struct {
@@ -57,21 +71,23 @@ type Person struct {
 	Name string
 }
 
-type Named interface {
-	~struct{ Name string }
-}
+// For reasons discussed in #51576, field accesses through type parameters will be disabled for Go 1.18.
 
-func SayName[T Named](t T) {
-	fmt.Println(t.Name)
-}
+// type Named interface {
+// 	~struct{ Name string }
+// }
 
-type NamedC interface {
-	// 如果不注释掉，会报错：(SayNameC: t.Name) t.Name undefined (interface NamedC has no method Name)
-	// struct {a int; Name string} |
-	// struct {b int; Name string} |
-	struct {c int; Name string} 
-}
+// func SayName[T Named](t T) {
+// 	fmt.Println(t.Name)
+// }
 
-func SayNameC[T NamedC](t T) {
-	fmt.Println(t.Name)
-}
+// type NamedC interface {
+// 	// 如果不注释掉，会报错：(SayNameC: t.Name) t.Name undefined (interface NamedC has no method Name)
+// 	// struct {a int; Name string} |
+// 	// struct {b int; Name string} |
+// 	struct {c int; Name string}
+// }
+
+// func SayNameC[T NamedC](t T) {
+// 	fmt.Println(t.Name)
+// }
