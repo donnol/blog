318cf2aa21bd1296d0c77173af3c0e87f0086346
donnol <donnol@outlook.com>
2022-04-29 17:11:52 +0800

    demo/go: add enum

---
+++ demo/go/enum/enum.go
@@ -0,0 +1,171 @@
+package enum
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"strings"
+)
+
+type EnumField[T comparable] struct {
+	value  T
+	name   string
+	zhName string
+}
+
+func (e EnumField[T]) Value() T {
+	return e.value
+}
+
+func (e EnumField[T]) Name() string {
+	return e.name
+}
+
+func (e EnumField[T]) ZhName() string {
+	return e.zhName
+}
+
+func Convert[T comparable, R any](ef EnumField[T], f func(EnumField[T]) R) R {
+	return f(ef)
+}
+
+type Enum[T comparable] struct {
+	fields []EnumField[T]
+
+	values   []T
+	fieldMap map[T]EnumField[T]
+}
+
+func (e Enum[T]) Values() []T {
+	return e.values
+}
+
+func (e Enum[T]) Fields() []EnumField[T] {
+	return e.fields
+}
+
+func (e Enum[T]) Map() map[T]EnumField[T] {
+	return e.fieldMap
+}
+
+func (e Enum[T]) NameByValue(key T) string {
+	return e.fieldMap[key].name
+}
+
+func (e Enum[T]) ZhNameByValue(key T) string {
+	return e.fieldMap[key].zhName
+}
+
+// Init 需要传入结构体指针实例，否则报错；将根据它的enum tag的值来初始化该实例
+// 因为枚举值一般只需一套固定值，所以全局初始化一个实例即可
+// 获取tag，为字段赋值，没有则报错
+func Init[E comparable](e any) error {
+	v := reflect.ValueOf(e)
+	t := v.Type()
+	if t.Kind() != reflect.Ptr {
+		return (fmt.Errorf("input value is not a pointer"))
+	}
+	ve := v.Elem()
+	te := t.Elem()
+	if te.Kind() != reflect.Struct {
+		return (fmt.Errorf("input value is not a pointer of struct"))
+	}
+	if te.NumField() == 0 {
+		return (fmt.Errorf("input value don't have field"))
+	}
+	var fieldValueType reflect.Type
+	for i := 0; i < te.NumField(); i++ {
+		field := te.Field(i)
+
+		if field.Anonymous {
+			continue
+		}
+
+		if field.Type.Kind() != reflect.Struct {
+			continue
+		}
+
+		for j := 0; j < field.Type.NumField(); j++ {
+			ff := field.Type.Field(j)
+			if ff.Name == "value" {
+				fieldValueType = ff.Type
+				break
+			}
+		}
+
+		if fieldValueType != nil {
+			break
+		}
+	}
+
+	var enumObj = Enum[E]{
+		fields:   make([]EnumField[E], 0, te.NumField()),
+		values:   make([]E, 0, te.NumField()),
+		fieldMap: make(map[E]EnumField[E], te.NumField()),
+	}
+	var enumValue reflect.Value
+	for i := 0; i < te.NumField(); i++ {
+		field := te.Field(i)
+
+		if field.Anonymous {
+			if field.Name == "Enum" {
+				enumValue = ve.Field(i)
+			}
+			continue
+		}
+
+		ev, ok := field.Tag.Lookup("enum")
+		if !ok {
+			return (fmt.Errorf("field %s don't have an enum tag", field.Name))
+		}
+
+		vf := ve.Field(i)
+
+		ef := EnumField[E]{}
+		evs := strings.Split(ev, ",")
+		if len(evs) == 0 {
+			return (fmt.Errorf("field tag don't have enough value set"))
+		}
+		var firstValue reflect.Value
+		switch fieldValueType.Kind() {
+		case reflect.Int:
+			v, err := strconv.Atoi(evs[0])
+			if err != nil {
+				return (err)
+			}
+			firstValue = reflect.ValueOf(v)
+		case reflect.String:
+			first := reflect.ValueOf(evs[0])
+			firstValue = first
+		default:
+			return fmt.Errorf("not support %v yet", fieldValueType.Kind())
+		}
+		fv := firstValue.Interface().(E)
+		enumObj.values = append(enumObj.values, fv)
+		switch len(evs) {
+		case 1:
+			ef.value = fv
+		case 2:
+			ef.value = fv
+			ef.name = firstValue.Interface().(string)
+			ef.zhName = evs[1]
+		case 3:
+			ef.value = fv
+			ef.name = evs[1]
+			ef.zhName = evs[2]
+		}
+		enumObj.fields = append(enumObj.fields, ef)
+
+		// 枚举值不能重复
+		if _, ok := enumObj.fieldMap[ef.value]; ok {
+			return fmt.Errorf("enum value %v already exist, please use another value", ef.value)
+		}
+		enumObj.fieldMap[ef.value] = ef
+
+		vf.Set(reflect.ValueOf(ef))
+	}
+
+	enumValue.Set(reflect.ValueOf(enumObj))
+
+	return nil
+}
---
+++ demo/go/enum/enum_test.go
@@ -0,0 +1,249 @@
+package enum
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+// 定义结构的同时，通过struct tag设置值
+// 字段，字段类型，[字段整形值、]字段英文值、字段中文值(第一个值将作为map的key)
+// 如果不内嵌Enum[T]，要怎么拿到枚举的集合呢？
+type (
+	Color = EnumField[int]
+)
+
+var (
+	ColorEnumObj struct {
+		Enum[int]
+		Blue  Color `enum:"1,blue,蓝色"`
+		Green Color `enum:"2,green,绿色"`
+		Red   Color `enum:"3,red,红色"`
+	}
+)
+
+// 考虑到结构体只需出现一次，直接使用匿名结构体，并且在定义结构体的同时声明变量
+var (
+	WeekdayEnumObj struct {
+		Enum[string]
+		Monday    EnumField[string] `enum:"monday,星期一"`
+		Tuesday   EnumField[string] `enum:"tuesday,星期二"`
+		Wednesday EnumField[string] `enum:"wednesday,星期三"`
+		Thursday  EnumField[string] `enum:"thursday,星期四"`
+		Friday    EnumField[string] `enum:"friday,星期五"`
+		Saturday  EnumField[string] `enum:"saturday,星期六"`
+		Sunday    EnumField[string] `enum:"sunday,星期日"`
+	}
+)
+
+// 初始化
+func init() {
+	panicIf(Init[int](&ColorEnumObj))
+
+	panicIf(Init[string](&WeekdayEnumObj))
+}
+
+func TestEnum(t *testing.T) {
+	for _, testCase := range []struct {
+		name     string
+		handlers []handler
+	}{
+		{"color", colorHandlers},
+		{"weekday", weekdayHandlers},
+	} {
+		t.Run(testCase.name, func(t *testing.T) {
+			for _, h := range testCase.handlers {
+				r := h.handler()
+				if !reflect.DeepEqual(r, h.want) {
+					t.Fatalf("bad case: %v != %v\n", r, h.want)
+				}
+			}
+		})
+	}
+}
+
+type (
+	handler struct {
+		handler func() any
+		want    any
+	}
+)
+
+var (
+	colorHandlers = []handler{
+		{func() any { return ColorEnumObj.Blue.Value() }, 1},
+		{func() any { return ColorEnumObj.Green.Value() }, 2},
+		{func() any { return ColorEnumObj.Red.Value() }, 3},
+		{func() any { return ColorEnumObj.Blue.Name() }, "blue"},
+		{func() any { return ColorEnumObj.Green.Name() }, "green"},
+		{func() any { return ColorEnumObj.Red.Name() }, "red"},
+		{func() any { return ColorEnumObj.Blue.ZhName() }, "蓝色"},
+		{func() any { return ColorEnumObj.Green.ZhName() }, "绿色"},
+		{func() any { return ColorEnumObj.Red.ZhName() }, "红色"},
+
+		{func() any { return ColorEnumObj.Values() }, []int{1, 2, 3}},
+		{func() any { return ColorEnumObj.Fields() }, []EnumField[int]{
+			{1, "blue", "蓝色"},
+			{2, "green", "绿色"},
+			{3, "red", "红色"},
+		}},
+		{func() any { return ColorEnumObj.Map() }, map[int]EnumField[int]{
+			1: {1, "blue", "蓝色"},
+			2: {2, "green", "绿色"},
+			3: {3, "red", "红色"},
+		}},
+		{func() any { return ColorEnumObj.NameByValue(ColorEnumObj.Blue.Value()) }, "blue"},
+		{func() any { return ColorEnumObj.NameByValue(ColorEnumObj.Green.Value()) }, "green"},
+		{func() any { return ColorEnumObj.NameByValue(ColorEnumObj.Red.Value()) }, "red"},
+		{func() any { return ColorEnumObj.ZhNameByValue(ColorEnumObj.Blue.Value()) }, "蓝色"},
+		{func() any { return ColorEnumObj.ZhNameByValue(ColorEnumObj.Green.Value()) }, "绿色"},
+		{func() any { return ColorEnumObj.ZhNameByValue(ColorEnumObj.Red.Value()) }, "红色"},
+	}
+
+	weekdayHandlers = []handler{
+		{func() any { return WeekdayEnumObj.Monday.Value() }, "monday"},
+		{func() any { return WeekdayEnumObj.Tuesday.Value() }, "tuesday"},
+		{func() any { return WeekdayEnumObj.Wednesday.Value() }, "wednesday"},
+		{func() any { return WeekdayEnumObj.Thursday.Value() }, "thursday"},
+		{func() any { return WeekdayEnumObj.Friday.Value() }, "friday"},
+		{func() any { return WeekdayEnumObj.Saturday.Value() }, "saturday"},
+		{func() any { return WeekdayEnumObj.Sunday.Value() }, "sunday"},
+		{func() any { return WeekdayEnumObj.Monday.Name() }, "monday"},
+		{func() any { return WeekdayEnumObj.Tuesday.Name() }, "tuesday"},
+		{func() any { return WeekdayEnumObj.Wednesday.Name() }, "wednesday"},
+		{func() any { return WeekdayEnumObj.Thursday.Name() }, "thursday"},
+		{func() any { return WeekdayEnumObj.Friday.Name() }, "friday"},
+		{func() any { return WeekdayEnumObj.Saturday.Name() }, "saturday"},
+		{func() any { return WeekdayEnumObj.Sunday.Name() }, "sunday"},
+		{func() any { return WeekdayEnumObj.Monday.ZhName() }, "星期一"},
+		{func() any { return WeekdayEnumObj.Tuesday.ZhName() }, "星期二"},
+		{func() any { return WeekdayEnumObj.Wednesday.ZhName() }, "星期三"},
+		{func() any { return WeekdayEnumObj.Thursday.ZhName() }, "星期四"},
+		{func() any { return WeekdayEnumObj.Friday.ZhName() }, "星期五"},
+		{func() any { return WeekdayEnumObj.Saturday.ZhName() }, "星期六"},
+		{func() any { return WeekdayEnumObj.Sunday.ZhName() }, "星期日"},
+
+		{func() any { return WeekdayEnumObj.Values() }, []string{"monday", "tuesday", "wednesday", "thursday", "friday", "saturday", "sunday"}},
+		{func() any { return WeekdayEnumObj.Fields() }, []EnumField[string]{
+			{"monday", "monday", "星期一"},
+			{"tuesday", "tuesday", "星期二"},
+			{"wednesday", "wednesday", "星期三"},
+			{"thursday", "thursday", "星期四"},
+			{"friday", "friday", "星期五"},
+			{"saturday", "saturday", "星期六"},
+			{"sunday", "sunday", "星期日"},
+		}},
+		{func() any { return WeekdayEnumObj.Map() }, map[string]EnumField[string]{
+			"monday":    {"monday", "monday", "星期一"},
+			"tuesday":   {"tuesday", "tuesday", "星期二"},
+			"wednesday": {"wednesday", "wednesday", "星期三"},
+			"thursday":  {"thursday", "thursday", "星期四"},
+			"friday":    {"friday", "friday", "星期五"},
+			"saturday":  {"saturday", "saturday", "星期六"},
+			"sunday":    {"sunday", "sunday", "星期日"},
+		}},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Monday.Value())
+		}, "monday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Tuesday.Value())
+		}, "tuesday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Wednesday.Value())
+		}, "wednesday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Thursday.Value())
+		}, "thursday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Friday.Value())
+		}, "friday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Saturday.Value())
+		}, "saturday"},
+		{func() any {
+			return WeekdayEnumObj.NameByValue(WeekdayEnumObj.Sunday.Value())
+		}, "sunday"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Monday.Value())
+		}, "星期一"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Tuesday.Value())
+		}, "星期二"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Wednesday.Value())
+		}, "星期三"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Thursday.Value())
+		}, "星期四"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Friday.Value())
+		}, "星期五"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Saturday.Value())
+		}, "星期六"},
+		{func() any {
+			return WeekdayEnumObj.ZhNameByValue(WeekdayEnumObj.Sunday.Value())
+		}, "星期日"},
+	}
+)
+
+func panicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+var (
+	ErrorEnumObj struct {
+		Enum[int]
+
+		Ok             EnumField[int] `enum:"0,ok,正常"`
+		BadParam       EnumField[int] `enum:"1,BadParam,参数错误"`
+		BusinessFailed EnumField[int] `enum:"2,BusinessFailed,业务执行失败"`
+		EncodeFailed   EnumField[int] `enum:"3,EncodeFailed,编码失败"`
+		DecodeFailed   EnumField[int] `enum:"4,DecodeFailed,解码失败"`
+	}
+)
+
+type (
+	Result struct {
+		Code int    `json:"code"`
+		Msg  string `json:"msg"`
+		Data any    `json:"data"`
+	}
+)
+
+func TestEnumError(t *testing.T) {
+	err := Init[int](&ErrorEnumObj)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	badParam := Convert(ErrorEnumObj.BadParam, func(ef EnumField[int]) error {
+		return fmt.Errorf("code: %v, msg: %s-%s", ef.Value(), ef.Name(), ef.ZhName())
+	})
+	t.Logf("badParam: %+v\n", badParam)
+
+	res := Convert(ErrorEnumObj.Ok, func(ef EnumField[int]) Result {
+		return Result{
+			Code: ef.value,
+			Msg:  ef.ZhName(),
+		}
+	})
+	t.Logf("res: %+v\n", res)
+}
+
+var (
+	repeatedEnumValue struct {
+		Enum[int]
+		First  EnumField[int] `enum:"1"`
+		Repeat EnumField[int] `enum:"1"`
+	}
+)
+
+func TestRepeatedEnum(t *testing.T) {
+	err := Init[int](&repeatedEnumValue)
+	if err.Error() != "enum value 1 already exist, please use another value" {
+		t.Fatal(err)
+	}
+}
