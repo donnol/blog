dfa6d652c9f2890fb0c0077d61ba371a3c1cdc5e
donnol <donnol@outlook.com>
2022-05-23 14:47:43 +0800

    demo/go: update grpc interceptor

--- demo/go/grpc/grpc.go
+++ demo/go/grpc/grpc.go
@@ -21,45 +21,67 @@ func Server() {
 
 	defer server.GracefulStop()
 
-	info := server.GetServiceInfo()
-	fmt.Printf("info: %+v\n", info)
-
 	// 可以调多次，注册多个服务
-	server.RegisterService(&grpc.ServiceDesc{
-		ServiceName: "pinger",
-		HandlerType: (*Pinger)(nil), // 接口指针
-		Methods: []grpc.MethodDesc{
-			{
-				MethodName: "ping",
-				Handler: func(srv interface{}, ctx context.Context, f func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
-					// srv是绑定的具体实例，如果有多个方法时，怎么对应呢？
-					// Methods是一个数组，多个方法对应到数组的元素里
-					fmt.Printf("srv: %v\n", srv)
+	server.RegisterService(
+		&grpc.ServiceDesc{
+			ServiceName: "pinger",
+			HandlerType: (*Pinger)(nil), // 接口指针
+			Methods: []grpc.MethodDesc{
+				{
+					MethodName: "ping",
+					Handler: func(srv interface{}, ctx context.Context, f func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+						// srv是绑定的具体实例，如果有多个方法时，怎么对应呢？
+						// Methods是一个数组，多个方法对应到数组的元素里
+						fmt.Printf("srv: %v\n", srv)
 
-					fmt.Printf("ctx: %+v\n", ctx)
+						fmt.Printf("ctx: %+v\n", ctx)
 
-					// 参数在哪里呢？
-					// 参数需要通过调用f来获取，如下：
-					// in := new(Param)
-					// if err := dec(in); err != nil {
-					// 	return nil, err
-					// }
+						// 参数在哪里呢？
+						// 参数需要通过调用f来获取，如下：
+						// in := new(Param)
+						// if err := dec(in); err != nil {
+						// 	return nil, err
+						// }
 
-					// 这里执行的f又是哪里来的呢？执行它又有什么用呢？
-					// 如上所述，f是用来获取参数的
-					var m = message("hello")
-					if err := f(&m); err != nil {
-						panic(err)
-					}
+						// 这里执行的f又是哪里来的呢？执行它又有什么用呢？
+						// 如上所述，f是用来获取参数的
+						var m message
+						if err := f(&m); err != nil {
+							panic(err)
+						}
 
-					r := message("good")
-					return &r, nil
+						// interceptor这样用？
+						// https://github.com/grpc/grpc-go/blob/master/examples/helloworld/helloworld/helloworld_grpc.pb.go#L75
+						if interceptor != nil {
+							info := &grpc.UnaryServerInfo{
+								Server:     srv,
+								FullMethod: "/pinger/ping",
+							}
+							handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+								in := req.(string)
+								r1 := srv.(Pinger).Ping(ctx, in)
+								return message(r1), nil
+							}
+							return interceptor(ctx, m.String(), info, handler)
+						}
+
+						// 对srv断言，获得具体类型，并执行方法
+						r1 := srv.(Pinger).Ping(ctx, m.String())
+
+						// 包装返回
+						r := message(r1)
+						return &r, nil
+					},
 				},
 			},
+			Streams:  nil,
+			Metadata: nil,
 		},
-		Streams:  nil,
-		Metadata: nil,
-	}, &pingImpl{name: "jd"})
+		&pingImpl{name: "jd"},
+	)
+
+	info := server.GetServiceInfo()
+	fmt.Printf("service info: %+v\n", info)
 
 	l, err := net.Listen("tcp", "127.0.0.1:8980")
 	if err != nil {
@@ -71,14 +93,14 @@ func Server() {
 }
 
 type Pinger interface {
-	Ping(msg string) string
+	Ping(ctx context.Context, msg string) string
 }
 
 type pingImpl struct {
 	name string
 }
 
-func (p *pingImpl) Ping(msg string) string {
+func (p *pingImpl) Ping(ctx context.Context, msg string) string {
 	fmt.Printf("msg: %s\n", msg)
 
 	return "I am " + p.name
--- demo/go/grpc/grpc_test.go
+++ demo/go/grpc/grpc_test.go
@@ -1,9 +1,13 @@
 package grpc
 
-import "testing"
+import (
+	"testing"
+	"time"
+)
 
 func TestGrpc(t *testing.T) {
 	go Server()
 
+	time.Sleep(100 * time.Millisecond)
 	Client()
 }
