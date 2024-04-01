fb6b631d31ccbcfd3b5086fb36b8b33f7edbc439
donnol <donnol@outlook.com>
2024-04-01 10:56:18 +0800

    demo(go): add str

---
+++ demo/go/str/match.go
@@ -0,0 +1,52 @@
+package str
+
+// Index 字符串匹配的KMP算法实现(O(n+m))
+func Index(s, pattern string) int {
+	nex := next(pattern)
+
+	tar, pos := 0, 0
+
+	for tar < len(s) {
+		if s[tar] == pattern[pos] {
+			tar++
+			pos++
+		} else if pos != 0 {
+			pos = nex[pos-1]
+		} else {
+			tar++
+		}
+
+		if pos == len(pattern) {
+			return tar - pos
+		}
+	}
+
+	return -1
+}
+
+// next 为子串建立next数组，在遍历过程中跳过不可能匹配上的内容，从而减少遍历次数
+// "部分匹配值"就是"前缀"和"后缀"的最长的共有元素的长度
+// next数组元素：代表当前字符之前的字符串中，"前缀"和"后缀"的最长的共有元素的长度
+// 可以确切地知道在当前位置之前的一个潜在匹配的位置
+func next(pattern string) []int {
+	r := make([]int, 0, len(pattern))
+
+	r = append(r, 0)
+	x := 1
+	now := 0
+
+	for x < len(pattern) {
+		if pattern[now] == pattern[x] {
+			now += 1
+			x += 1
+			r = append(r, now)
+		} else if now != 0 {
+			now = r[now-1]
+		} else {
+			r = append(r, 0)
+			x += 1
+		}
+	}
+
+	return r
+}
---
+++ demo/go/str/match_test.go
@@ -0,0 +1,108 @@
+package str
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestIndex(t *testing.T) {
+	type args struct {
+		s       string
+		pattern string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				s:       "abcdfdfdfdf",
+				pattern: "dfd", // pattern dfd 's next array [0 0 1]
+			},
+			want: 3,
+		},
+		{
+			name: "2",
+			args: args{
+				s:       "abcdefghijk",
+				pattern: "ijk", // pattern ijk 's next array [0 0 0]
+			},
+			want: 8,
+		},
+		{
+			name: "3",
+			args: args{
+				s:       "abcdefghijkOKM1232323232dfdfdfEFEFEFAERER",
+				pattern: "AER", // pattern AER 's next array [0 0 0]
+			},
+			want: 36,
+		},
+		{
+			name: "4",
+			args: args{
+				s:       "abcdefghijkOKM1232323232dfdfdfEFEFEFAERER",
+				pattern: "dcb", // pattern dcb 's next array [0 0 0]
+			},
+			want: -1,
+		},
+		{
+			name: "5",
+			args: args{
+				s:       "abcdefghijkOKM1232323232dfdfdfEFEFEFAERER",
+				pattern: "abc", // pattern abc 's next array [0 0 0]
+			},
+			want: 0,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			{
+				var got int
+				if got = Index(tt.args.s, tt.args.pattern); got != tt.want {
+					t.Errorf("Index() = %v, want %v", got, tt.want)
+				}
+				if got != -1 {
+					if tt.args.s[got:got+len(tt.args.pattern)] != tt.args.pattern {
+						t.Errorf("string slice = %v, want %v, ", tt.args.s[got:got+len(tt.args.pattern)], tt.args.pattern)
+					}
+				}
+			}
+			{
+				var got int
+				if got = strings.Index(tt.args.s, tt.args.pattern); got != tt.want {
+					t.Errorf("Index() = %v, want %v", got, tt.want)
+				}
+				if got != -1 {
+					if tt.args.s[got:got+len(tt.args.pattern)] != tt.args.pattern {
+						t.Errorf("string slice = %v, want %v, ", tt.args.s[got:got+len(tt.args.pattern)], tt.args.pattern)
+					}
+				}
+			}
+		})
+	}
+}
+
+// BenchmarkIndex-4
+// 15456728        76.65 ns/op      24 B/op       1 allocs/op
+func BenchmarkIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Index(
+			"abcdefghijkOKM1232323232dfdfdfEFEFEFAERER",
+			"AER",
+		)
+	}
+}
+
+// BenchmarkStdIndex-4
+// 155435454         7.732 ns/op       0 B/op       0 allocs/op
+func BenchmarkStdIndex(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		strings.Index(
+			"abcdefghijkOKM1232323232dfdfdfEFEFEFAERER",
+			"AER",
+		)
+	}
+}
