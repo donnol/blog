531514318fd74b30ce6215db33251f875bfd475a
donnol <donnol@outlook.com>
2024-05-29 10:19:41 +0800

    demo(go): update

--- demo/go/leetcode/maxlen_of_strings.go
+++ demo/go/leetcode/maxlen_of_strings.go
@@ -35,3 +35,70 @@ func haveSameChar(s1, s2 string) bool {
 	}
 	return false
 }
+
+func MaxProduct(s []string) int {
+	n := len(s)
+	dp := make([][]int, n)
+	for i := 0; i < n; i++ {
+		dp[i] = make([]int, n)
+	}
+
+	masks := make([]int64, n)
+	for i := range s {
+		for j := range s[i] {
+			masks[i] |= 1 << (s[i][j] - 'a')
+		}
+	}
+
+	for i := 0; i < n; i++ {
+		for j := i + 1; j < n; j++ {
+			if masks[i]&masks[j] == 0 {
+				dp[i][j] = len(s[i]) * len(s[j])
+			}
+			ai, aj := i-1, j-1
+			if ai < 0 {
+				ai = 0
+			}
+			if aj < 0 {
+				aj = 0
+			}
+			dp[i][j] = max(dp[i][j], dp[ai][j], dp[i][aj])
+		}
+	}
+
+	return dp[n-2][n-1]
+}
+
+func max(s ...int) int {
+	max := 0
+	for _, v := range s {
+		if v > max {
+			max = v
+		}
+	}
+	return max
+}
+
+// def maxProduct(words):
+//     n = len(words)
+//     masks = [0] * n  # 存储每个字符串的位掩码
+//     dp = [[0] * n for _ in range(n)]  # 动态规划表
+
+//     # 计算每个字符串的位掩码
+//     for i, word in enumerate(words):
+//         for c in word:
+//             masks[i] |= 1 << (ord(c) - ord('a'))
+
+//     # 动态规划
+//     for i in range(n):
+//         for j in range(i + 1, n):
+//             if masks[i] & masks[j] == 0:  # 检查位掩码是否有交集
+//                 dp[i][j] = len(words[i]) * len(words[j])
+//             dp[i][j] = max(dp[i][j], dp[i-1][j], dp[i][j-1])  # 更新最大值
+
+//     return dp[n - 2][n - 1]
+
+// # 示例
+// words = ["abcw", "baz", "foo", "bar", "xtfn", "abcdef"]
+// result = maxProduct(words)
+// print(result)  # 输出：16
--- demo/go/leetcode/maxlen_of_strings_test.go
+++ demo/go/leetcode/maxlen_of_strings_test.go
@@ -1,6 +1,8 @@
 package leetcode
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestMaxLen(t *testing.T) {
 	type args struct {
@@ -24,6 +26,33 @@ func TestMaxLen(t *testing.T) {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := MaxLen(tt.args.s); got != tt.want {
 				t.Errorf("MaxLen() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestMaxProduct(t *testing.T) {
+	type args struct {
+		s []string
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		// TODO: Add test cases.
+		{
+			name: "1",
+			args: args{
+				s: []string{"abcw", "baz", "foo", "bar", "xtfn", "abcdef"},
+			},
+			want: 16,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := MaxProduct(tt.args.s); got != tt.want {
+				t.Errorf("MaxProduct() = %v, want %v", got, tt.want)
 			}
 		})
 	}
